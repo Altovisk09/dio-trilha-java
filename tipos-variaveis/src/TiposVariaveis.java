c8872a2e8b2bb6641f021c1d4ba4d3ecfabf6e82
Altovisk09 <ericaltao13@gmail.com>
2025-03-24 18:19:13 -0300

    tipos primitivos de dados

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,14 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    double salarioMinimo =  2500.53;
+    short numeroBaixo = 1;
+    int numeroNormal = numeroBaixo;
+    short casting = (short) numeroNormal;
+
+    int valorMutavel = 10;
+    valorMutavel = 5;
+
+    final double PI = 3.14; 
     }
 }
+
